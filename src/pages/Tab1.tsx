dfa7149369e35bbe4d0de7369adcffe8592a5358
VallejoCristopher <cristopher.vallejo@uisek.edu.ec>
2025-12-03 18:29:33 -0500

    laboratorio7 Cristopher Vallejo

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,4 +1,5 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import {IonContent,IonHeader,IonPage,IonTitle,IonToolbar,IonCard,IonCardContent,IonCardHeader,IonCardSubtitle,IonCardTitle,
+}from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
 
@@ -16,7 +17,17 @@ const Tab1: React.FC = () => {
             <IonTitle size="large">Tab 1</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
+         <IonCard>
+        <img alt="Silhouette of mountains" src="https://ionicframework.com/docs/img/demos/card-media.png" />
+        <IonCardHeader>
+          <IonCardTitle>Cristopher Vallejo</IonCardTitle>
+          <IonCardSubtitle>Desarrollo Móvil</IonCardSubtitle>
+        </IonCardHeader>
+
+      <IonCardContent>
+        El framework Ionic permite crear aplicaciones móviles y de escritorio utilizando tecnologías web como HTML, CSS y JavaScript, facilitando el desarrollo multiplataforma con una sola base de código.
+      </IonCardContent>
+    </IonCard>
       </IonContent>
     </IonPage>
   );
